0353297be8c603c3357324911bc06a5fdffc7eef
tomsato <tmhr.st9711@gmail.com>
2016-02-28 19:34:18 +0900

    add test

---
+++ Arithmetic.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 足し算引き算を行うクラス
+ */
+class Arithmetic
+{
+    /**
+     * 足し算
+     */
+    public function add($x, $y) {
+        return($x + $y);
+    }
+ 
+    /**
+     * 引き算
+     */
+    public function subtract($x, $y) {
+        return($x - $y);
+    }
+}
